d67c26b3d84c98ca7f4ef344e8c363d08c7f5b00
Renan Kogan <43600986+HipotenusaBR@users.noreply.github.com>
2022-05-03 04:47:16 -0300

    Delete Book.java

--- Book.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-
-public record Book(String title, String author, String publisher, LocalDate releaseDate) {
-
-	public Book(String title, String author, String publisher) {
-		this(title, author, publisher, LocalDate.now());
-	}
-	
-}
